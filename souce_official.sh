54d3a7b9e12e4573ef9bfe279b3cc2cc0c65ff28
kodishmediacenter <fabiolmglgeo@gmail.com>
2019-11-14 18:49:05 -0300

    Create souce_official.sh

---
+++ souce_official.sh
@@ -0,0 +1,7 @@
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:team-xbmc/ppa -y
+sudo apt-get update -y
+sudo apt-get install kodi -y
+sudo apt-get install kodi-pvr-* -y
+sudo apt-get install kodi-audioencoder-* -y
+sudo apt-get install kodi-game-* -y
