fba0d9365199e1500b869a7309b8aab337b0eed0
kodishmediacenter <fabiolmglgeo@gmail.com>
2019-11-17 16:06:58 -0300

    Update souce_official.sh

--- souce_official.sh
+++ souce_official.sh
@@ -26,7 +26,6 @@ sudo apt-get install kodi-pvr-* -y
 sudo apt-get install kodi-audioencoder-* -y
 sudo apt-get install kodi-game-* -y
 sudo apt-get install vlc -y
-sudo snap install spotify
 wget https://dl.strem.io/linux/v4.4.54/stremio_4.4.52-1_amd64.deb
 sudo apt-get install -f
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -35,6 +34,10 @@ sudo apt-get install unzip
 mkdir /opt/kodish
 chmod 777 /opt/kodish
 
+# mudei para deb o spotify
+wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.1.10.546.ge08ef575-19_amd64.deb
+dpkg -i spotify-client_1.1.10.546.ge08ef575-19_amd64.deb
+apt-get install -f
 
 # instalando os serviços que kodish usa
 
