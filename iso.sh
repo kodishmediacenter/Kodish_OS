230d919c367cf8cb9e5ecdcbbcc518bd6d302674
kodishmediacenter <fabiolmglgeo@gmail.com>
2020-03-16 17:55:53 -0300

    Create iso.sh

---
+++ iso.sh
@@ -0,0 +1,4 @@
+wget https://sourceforge.net/projects/pinguy-os/files/ISO_Builder/pinguybuilder_5.1-8_all.deb/download
+mv pinguybuilder_5.1-8_all.deb pinguybuilder.deb
+sudo dpkg -i pinguybuilder.deb
+sudo apt-get install -f
