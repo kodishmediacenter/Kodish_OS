3d74c873a0ecbfff797d141298ff8b87ac705d2e
kodishmediacenter <fabiolmglgeo@gmail.com>
2020-04-05 11:02:07 -0300

    Update kodish_os_desktop.sh

--- kodish_os_desktop.sh
+++ kodish_os_desktop.sh
@@ -29,18 +29,30 @@ sudo apt-get install vlc -y
 wget https://dl.strem.io/linux/v4.4.54/stremio_4.4.52-1_amd64.deb
 sudo apt-get install -f
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get install unzip
-mkdir /opt/kodish
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install unzip -y
+sudo mkdir /opt/kodish
 chmod 777 /opt/kodish
 
 # mudei para deb o spotify
 wget http://repository.spotify.com/pool/non-free/s/spotify-client/spotify-client_1.1.10.546.ge08ef575-19_amd64.deb
-dpkg -i spotify-client_1.1.10.546.ge08ef575-19_amd64.deb
-apt-get install -f
+sudo dpkg -i spotify-client_1.1.10.546.ge08ef575-19_amd64.deb
+sudo apt-get install -f
 
 # instalando os serviços que kodish usa
 
 sudo apt-get install samba -y
 sudo apt-get install git -y 
+
+# atualizando o kodi para para portugues
+cd /usr/share/kodi/addons/
+mkdir plugin.executable.kodishmenu
+cd plugin.executable.kodishmenu
+
+wget https://raw.githubusercontent.com/kodishmediacenter/Kodish_OS/master/plugin.executable.kodishmenu/icon.png
+wget https://raw.githubusercontent.com/kodishmediacenter/Kodish_OS/master/plugin.executable.kodishmenu/addon.xml
+wget https://raw.githubusercontent.com/kodishmediacenter/Kodish_OS/master/plugin.executable.kodishmenu/default.py
+wget https://raw.githubusercontent.com/kodishmediacenter/Kodish_OS/master/plugin.executable.kodishmenu/stremio.sh
+cd ..
+
 
