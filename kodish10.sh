b37bf6e39764423cab527d1267798d246e1bb19d
kodishmediacenter <fabiolmglgeo@gmail.com>
2024-04-28 12:47:18 -0300

    Create kodish10.sh

---
+++ kodish10.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install flathub com.google.Chrome -y
+sudo flatpak install flathub com.stremio.Stremio -y
+sudo flatpak install flathub com.spotify.Client -y
+sudo flatpak install flathub com.github.tchx84.Flatseal -y
+sudo flatpak install flathub io.github.yuki_iptv.yuki-iptv -y
+sudo flatpak install flathub ca.littlesvr.asunder -y
+sudo flatpak install flathub fr.handbrake.ghb -y 
+sudo flatpak install flathub app.ytmdesktop.ytmdesktop -y
+sudo flatpak install flathub tv.kodi.Kodi -y
+sudo flatpak install flathub com.valvesoftware.Steam -y 
+sudo flatpak install flathub net.mkiol.Jupii -y
