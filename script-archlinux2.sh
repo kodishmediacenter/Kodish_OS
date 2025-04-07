06cc1015a44146df515051fdce213c3fedebcd49
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-04-07 20:08:09 -0300

    Update and rename script-archlinux.sh to script-archlinux2.sh

--- script-archlinux2.sh
+++ script-archlinux2.sh
@@ -2,32 +2,43 @@
 
 set -e
 
-# Teclado ABNT2 e NTP
+# Teclado e NTP
 loadkeys br-abnt2
 timedatectl set-ntp true
 
-# Particionamento em UEFI (APAGA TUDO EM /dev/sda)
-echo "Particionando disco /dev/sda..."
-parted /dev/sda --script mklabel gpt
-parted /dev/sda --script mkpart ESP fat32 1MiB 513MiB
-parted /dev/sda --script set 1 esp on
-parted /dev/sda --script mkpart primary ext4 513MiB 100%
+# Pergunta o disco a ser usado
+read -rp "Digite o disco para instalar (ex: /dev/sda, /dev/nvme0n1): " DISCO
+
+# Confirmação
+echo "⚠️  TODOS OS DADOS EM $DISCO SERÃO APAGADOS!"
+read -rp "Deseja continuar? (s/N): " CONFIRMA
+[[ "$CONFIRMA" != "s" && "$CONFIRMA" != "S" ]] && exit 1
+
+# Particionamento UEFI
+echo "Particionando $DISCO..."
+parted "$DISCO" --script mklabel gpt
+parted "$DISCO" --script mkpart ESP fat32 1MiB 513MiB
+parted "$DISCO" --script set 1 esp on
+parted "$DISCO" --script mkpart primary ext4 513MiB 100%
+
+# Define partições
+EFI="${DISCO}1"
+ROOT="${DISCO}2"
 
 # Formatação
-mkfs.fat -F32 /dev/sda1
-mkfs.ext4 /dev/sda2
+mkfs.fat -F32 "$EFI"
+mkfs.ext4 "$ROOT"
 
 # Montagem
-mount /dev/sda2 /mnt
-mkdir /mnt/boot
-mkdir /mnt/boot/efi
-mount /dev/sda1 /mnt/boot/efi
+mount "$ROOT" /mnt
+mkdir -p /mnt/boot/efi
+mount "$EFI" /mnt/boot/efi
 
 # Mirrorlist BR
 pacman -Sy --noconfirm reflector
 reflector --country Brazil --protocol https --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
-# Instalação básica
+# Instalação base
 pacstrap /mnt base linux linux-firmware sudo vim networkmanager grub efibootmgr
 
 # fstab
@@ -42,20 +53,16 @@ locale-gen
 echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
 echo "archlinux" > /etc/hostname
 
-# NetworkManager
 systemctl enable NetworkManager
 
-# Usuário padrão
-useradd -m -G wheel -s /bin/bash kodish
-echo "kodish:kodish" | chpasswd
+useradd -m -G wheel -s /bin/bash camila
+echo "camila:123456" | chpasswd
 echo "root:root" | chpasswd
 
-# Sudo
 echo '%wheel ALL=(ALL:ALL) ALL' > /etc/sudoers.d/wheel
 
-# GRUB UEFI
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
 
-echo "Instalação concluída com sucesso! Pode reiniciar."
+echo "✅ Instalação finalizada. Você pode reiniciar."
