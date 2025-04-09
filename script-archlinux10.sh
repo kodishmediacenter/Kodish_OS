bd611955700ef878574fe0e0c857d463a726b713
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-04-09 13:04:17 -0300

    Update script-archlinux10.sh

--- script-archlinux10.sh
+++ script-archlinux10.sh
@@ -139,6 +139,8 @@ systemctl enable lightdm
 # Extras
 pacman -S --noconfirm alsa-utils
 pacman -S --noconfirm pipewire pipewire-pulse wireplumber zenity jq lutris
+pacman -S --noconfirm noto-fonts-cjk
+
 systemctl --user enable --now pipewire pipewire-pulse wireplumber
 
 # Flatpak 
