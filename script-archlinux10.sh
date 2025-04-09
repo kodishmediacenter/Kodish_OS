e0f741765ac99dfcd79b1ec300e94ce62f85c439
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-04-08 21:45:05 -0300

    Update script-archlinux10.sh

--- script-archlinux10.sh
+++ script-archlinux10.sh
@@ -47,10 +47,6 @@ pacman -Sy archlinux-keyring --noconfirm
 rm -f /var/cache/pacman/pkg/*.part
 rm -f /var/cache/pacman/pkg/libspqr* /var/cache/pacman/pkg/libbgm*
 pacman -Syyu --noconfirm
-pacman-key --init
-pacman-key --populate archlinux
-pacman-key --refresh-keys
-
 pacstrap /mnt base linux linux-firmware vim sudo networkmanager grub efibootmgr os-prober mtools dosfstools
 
 # fstab
