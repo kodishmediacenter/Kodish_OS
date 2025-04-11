ba19e9ba2956a9688c9156e6acd50e63481a6105
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-04-11 14:45:19 -0300

    Update script-archlinux10.sh

--- script-archlinux10.sh
+++ script-archlinux10.sh
@@ -148,6 +148,7 @@ flatpak install flathub com.google.Chrome
 flatpak install flathub com.heroicgameslauncher.hgl
 flatpak install flathub com.vysp3r.ProtonPlus
 #flatpak install flathub net.retrodeck.retrodeck
+flatpak install flathub com.stremio.Stremio
 
 # Remover pacotes 
 pacman -Rs xfce4-dict 
