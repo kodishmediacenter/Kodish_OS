4e3cfd0e1ef27713c945560d4bd9c679a69ce2ef
kodishmediacenter <fabiolmglgeo@gmail.com>
2025-04-13 11:06:57 -0300

    Create flatpaks.sh

---
+++ flatpaks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Flatpak 
+flatpak install flathub com.google.Chrome
+flatpak install flathub com.heroicgameslauncher.hgl
+flatpak install flathub com.vysp3r.ProtonPlus
+#flatpak install flathub net.retrodeck.retrodeck
+flatpak install flathub com.stremio.Stremio
+flatpak install flathub dev.aunetx.deezer
+flatpak install flathub com.github.louis77.tuner
+
